97f9b915579e3ed1026ba594a450ae9598c47290
roseee123 <m10702150@gapps.ntust.edu.tw>
2019-07-03 14:21:35 +0800

    second Angular project -make message board on web -https://ithelp.ithome.com.tw/articles/10205162 -[Angular 深入淺出三十天] Day 09 - Angular 小學堂（二）Leo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { stationList} from './station-list.const';
+import { Message} from './message';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,18 @@ import { stationList} from './station-list.const';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // title = 'HelloAngular';
-  list = stationList;
+  title = 'HelloAngular';
+  name1 = '';
+  content = '';
+  messages: Message[] = [];
+  addMessage(): void {
+    if (!this.name1.trim() || !this.content.trim())
+    {
+      return;
+    }
+    const message = new Message(this.name1, this.content);
+    this.messages.push(message);
+    this.content ='';
+  }
+
 }
